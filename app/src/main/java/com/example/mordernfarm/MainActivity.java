28c28d88bcba002473a6980fb03e40aa52ca8db6
young <younggi960421@gmail.com>
2020-11-17 17:10:48 +0900

    블루투스

--- app/src/main/java/com/example/mordernfarm/MainActivity.java
+++ app/src/main/java/com/example/mordernfarm/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.mordernfarm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(
                         getApplicationContext(),
-                        blue.class); 
+                        bluetooth.class);
                 startActivity(intent);
             }
         });
